22e94f3b1dc9624f5d8ec57eb7190334a77c6192
wheelchaired_potato <marro.simone@iisgalvanimi.edu.it>
2025-05-16 20:57:43 +0000

    3° inizio home

--- Portfolio/src/app/home/home.component.ts
+++ Portfolio/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,21 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('fullDiv') fullDiv!: ElementRef;
 
+  ngAfterViewInit(): void {
+    this.setFullHeight();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.setFullHeight();
+  }
+
+  setFullHeight() {
+    const windowHeight = window.innerHeight;
+    this.fullDiv.nativeElement.style.height = `${windowHeight}px`;
+  }
 }
+
